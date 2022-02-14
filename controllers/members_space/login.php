a310aafb7d35a25b39577b83c839c4a3a691825a
Excellence KAWEJ <19ka201@esisalama.org>
2022-02-14 19:44:41 +0200

    ajout des commentaires dans controllers/login.php

--- controllers/members_space/login.php
+++ controllers/members_space/login.php
@@ -5,20 +5,25 @@ include_once('../../models/connexion_sql.php');
 include_once('../../models/members_space/db_operations.php');
 include_once('../../views/members_space/login.php');
 
-if(!isset($_POST)){
+
+// verifie les champs de la page de login sont tous remplis
+if(!empty($_POST['nickname']) && !empty($_POST['pwd'])){
     include_once('../../views/members_space/login.php');
     echo '<br>Veuillez compléter tous les champs.<br>';
 }else{
     $nickname = htmlspecialchars($_POST['nickname']);
     $pwd = htmlspecialchars($_POST['pwd']);
+    //verifie s'il existe des cookies sur la machine pour une rédirection vers le blog
     if(!empty($_COOKIE['nickname']) && !empty($_COOKIE['pwd'])){
         header('Location: ../blog/index.php');
     }elseif((!empty($pwd) && !empty($nickname))){
         $pwd = sha1('gz' . $pwd);
+        //verifie si les données correspondent à celles contenues dans la base de données
         if(
             in_array($nickname, verify_members($nickname, $pwd)[0]) &&
             in_array($pwd, verify_members($nickname, $pwd)[0])
         ){
+            //definir un cookie si la connexion automatique est définie
             if (isset($_POST['auto'])){
                 setcookie('nickname', $_COOKIE['nickname'], time() + 24 * 3600);
                 setcookie('pwd', $_COOKIE['pwd'], time() + 24 * 3600);
--- controllers/minichat/index.php
+++ controllers/minichat/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include_once('../../models/connexion_sql.php');
 include_once('../../models/minichat/db_operation.php');
 include_once('../../views/minichat/index.php');
@@ -13,10 +14,12 @@ if (empty($data)){
     }
 }
 
-if (!empty($_POST['pseudo']) && !empty($_POST['message']) && !in_array()){
-    add_message($_POST['pseudo'], $_POST['message']);
+if (!empty($_SESSION['nickname'])){
+    if (!empty($_POST['pseudo']) && !empty($_POST['message'])){
+        add_message($_POST['pseudo'], $_POST['message']);
+    }else{
+        echo ' Veuillez remplir tous les champs';
+    }
 }else{
-   echo 'Veuillez remplir tous les champs';
+    header('Location: ../members_space/login.php');
 }
-
-// while($data)
