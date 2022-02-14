459aebddd3d11206f467248d46f5739f02081752
Excellence KAWEJ <19ka201@esisalama.org>
2022-02-14 20:09:15 +0200

    ajout des commentaires dans controllers/minichat/index.php

--- controllers/minichat/index.php
+++ controllers/minichat/index.php
@@ -1,19 +1,24 @@
 <?php
+
 session_start();
+
 include_once('../../models/connexion_sql.php');
 include_once('../../models/minichat/db_operation.php');
 include_once('../../views/minichat/index.php');
 
-
 $data = message();
+
+//vérifie si la table n'est pas vide
 if (empty($data)){
     echo 'Chat vide';
 }else{
+    //affichage des données de la table
     foreach ($data as $datum){
            echo '<p><strong>' . htmlspecialchars($datum['nickname']) . '</strong> : ' . htmlspecialchars($datum['message']) . '</p>';
     }
 }
 
+//vérifie si la session n'est pas vide
 if (!empty($_SESSION['nickname'])){
     if (!empty($_POST['pseudo']) && !empty($_POST['message'])){
         add_message($_POST['pseudo'], $_POST['message']);
