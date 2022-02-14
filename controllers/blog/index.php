e65076aca6d479c31f5873aa7bc2e87d31ac1adc
Excellence KAWEJ <19ka201@esisalama.org>
2022-02-14 20:06:36 +0200

    ajout des commentaires dans controllers/members_space/subscription.php

--- controllers/blog/index.php
+++ controllers/blog/index.php
@@ -1,5 +1,7 @@
 <?php
+
 session_start();
+
 include_once('../../models/connexion_sql.php');
 include_once('../../models/blog/get_billets.php');
 include_once('../billets.php');
--- controllers/members_space/subscription.php
+++ controllers/members_space/subscription.php
@@ -1,15 +1,22 @@
 <?php
+
+session_start();
+
 include_once('../../models/connexion_sql.php');
 include_once('../../models/members_space/db_operations.php');
 include_once('../../views/members_space/subscription.php');
 
+//vérifier si les champs du formulaire d'inscription ne sont pas vides
 if (isset($_POST) && (!empty($_POST['nickname']) && !empty($_POST['pwd']) && !empty($_POST['pwd1']) && !empty($_POST['email']))) {
+    //vérifier si les mots de passes correspondent
     if ($_POST['pwd'] == $_POST['pwd1']) {
+        //vérifier la validité de l'email
         if (preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['email'])){
+            //vérifier si le pseudonyme exite dans la base de données
             try {
                 $pwd = sha1('gz' . $_POST['pwd']);
                 add_members($_POST['nickname'], $pwd, $_POST['email']);
-                session_start();
+
                 $data = verify_members($_POST['nickname'], $pwd);
                 $_SESSION['id'] = $data[0]['id'];
                 $_SESSION['nickname'] = $data[0]['nickname'];
