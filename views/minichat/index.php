acc57034b057360b522e75cf34ba57376f23ca6a
Excellence KAWEJ <19ka201@esisalama.org>
2022-02-24 13:15:02 +0200

    insertion si un utilisateur est connecté

--- views/minichat/index.php
+++ views/minichat/index.php
@@ -11,7 +11,7 @@
 <form action="../../controllers/minichat/index.php" method="post">
     <div>
         <label>
-            Pseudo : <input type="text" name="nickname" <?php echo (); ?>>
+            Pseudo : <input type="text" name="nickname" value="<?php echo (!empty($_SESSION['nickname'])) ? $_SESSION['nickname'] : ''; ?>">
         </label>
     </div><br>
     <div>
