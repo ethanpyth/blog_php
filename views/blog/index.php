6bdd790dab4f92c15e3c38bda9dec4776ccb39a0
Excellence KAWEJ <19ka201@esisalama.org>
2022-02-26 23:08:32 +0200

    stylisation de la page d' accueil du blog

--- views/blog/index.php
+++ views/blog/index.php
@@ -2,30 +2,42 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" href="../../views/bootstrap/css/bootstrap.css">
         <meta charset="UTF-8">
-        <title>Mon blog</title>
-        <link rel="stylesheet" href="../../views/blog/style.css">
+        <title>Accueil</title>
     </head>
     <body>
-        <h1>FORSYTHE BLOG</h1>
-        <p>Derniers billets du blog :</p>
-        <?php
-            for($index = 0; $index < count($billets); $index++)
-            {
+        <div class="row">
+            <nav class="navbar navbar-inverse col-sm-2">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="../../controllers/blog/index.php">Accueil</a></li>
+                    <li><a href="../../controllers/minichat/index.php">Minichat</a></li>
+                    <li><a href=""></a></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </div>
+        <h1 class="Page-header text-center">FORSYTHE BLOG</h1>
+        <div class="container">
+            <p>Derniers billets du blog :</p>
+            <?php
+                for($index = 0; $index < count($billets); $index++)
+                {
+                ?>
+                    <div class="text-black">
+                        <h3 class="text-center">
+                            <?php echo $billets[$index]['titre']; ?>
+                        </h3>
+                        <p class="text-center">
+                            <?php echo $billets[$index]['contenu']; ?><br>
+                            <em>le <?php echo $billets[$index]['date_creation_fr']; ?></em>
+                        </p>
+                        <em><a href="../../controllers/blog/commentaires.php?billet=<?php echo $billets[$index]['id'];?>">Commentaires</a></em>
+                        <hr>
+                    </div>
+            <?php
+                }
             ?>
-                <div class="news">
-                    <h3>
-                        <?php echo $billets[$index]['titre']; ?>
-                    </h3>
-                    <p>
-                        <?php echo $billets[$index]['contenu']; ?><br>
-                        <em>le <?php echo $billets[$index]['date_creation_fr']; ?></em>
-                    </p>
-                    <em><a href="../../controllers/blog/commentaires.php?billet=<?php echo $billets[$index]['id'];?>">Commentaires</a></em>
-                    <hr>
-                </div>
-        <?php
-            }
-        ?>
+        </div>
     </body>
 </html>
